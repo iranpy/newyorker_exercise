2a73845a33d080b9c59b05297d2dbb4cd699df81
ali <aebtehaj@protonmail.com>
2023-12-07 19:00:59 +0330

    pricing exercise

---
+++ setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+requires = [
+    'pymc',
+    'pytorch-forecasting',
+    'seaborn',
+    'jupyterlab'
+    ]
+
+tests_require = ['pytest']
+
+long_description = """
+    Price Excercise for Newyorker.
+"""
+
+setup(
+    name="newyorker",
+    version='0.1.0',
+    author="Ali Ebtehaj",
+    author_email="debian1390@gmail.com",
+    long_description=long_description,
+    python_requires="==3.10.*",
+    install_requires = requires,
+    tests_require=tests_require,
+    extras_require={
+          'test': tests_require,
+    },
+    packages=find_packages(),
+)
